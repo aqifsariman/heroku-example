fe7a601c62cf415fe05bd085cfc84d923ff43969
aqifsariman <aqifsariman@gmail.com>
2022-04-25 16:48:12 +0800

    Changed port

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-import express from 'express';
+import express from "express";
 
-const PORT = 3004;
+const PORT = process.env.PORT || 3004;
 
 // Initialise Express
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/bananas', (request, response) => {
-
+app.get("/bananas", (request, response) => {
   const responseText = `This is a random number: ${Math.random()}`;
 
-  console.log('request came in', responseText);
+  console.log("request came in", responseText);
 
   const data = { responseText };
 
-  response.render('bananas', data);
+  response.render("bananas", data);
 });
 
 app.listen(PORT);
